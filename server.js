d13df0651714c5bdb04f91387cf2424b87131066
Marek Jelen <marek@jelen.biz>
2011-11-23 20:35:39 +0100

    Logging, file listing

--- server.js
+++ server.js
@@ -17,37 +17,64 @@ var mongo = require('mongodb');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var Path = require('path');
+var winston = require('winston');
+
+winston.cli();
+winston.info('Bootstraping');
 
 var start = function(db, Config){
-
+  var ruid = 0;
+  var get_ruid = function(){
+    ruid++;
+    if(ruid == 5000) ruid = 1;
+    return ruid;
+  };
   app = http.createServer(function(request, response){
-    console.log('New request');
+    var ruid = get_ruid();
+    winston.info('(' + ruid + ') New connection');
+    // Parse URI
     uri = url.parse(request.url, true);
     // ID of application
     var appid = request.headers['x-appid'];
-    console.log('Appid: ' + appid);
+    winston.info('(' + ruid + ') Requests Appid = ' + appid);
     // Path to save to
     var path = appid + ":" + uri.pathname;
-    // Get file
     if(request.method == "GET"){
-      var gs = new mongo.GridStore(db, path, "r");
-      gs.open(function(error, file){
-        console.log('Downloading file: ' + error);
-        response.writeHead(200, {
-          'Content-length': file.length
+      if(uri.pathname == "/_list_files"){
+        winston.info('(' + ruid + ') Listing files');
+        db.collection('fs.files', function(error, collection){
+          collection.find({ filename: new RegExp('^' + appid + ":") }, function(error, cursor){
+            cursor.each(function(error, file){
+              if(file == null){
+                response.end();
+                winston.info('(' + ruid + ') Listing done');
+              }else{
+                winston.info('(' + ruid + ') File ' + file.filename); 
+                response.write(file.filename + "\n");
+              };
+            });
+          });
         });
-        stream = file.stream(true);
-        stream.on('data', function(data){
-          console.log('Reading data.');
-          response.write(data);
+      }else{
+        var gs = new mongo.GridStore(db, path, "r");
+        gs.open(function(error, file){
+          winston.info('(' + ruid + ') Downloading file ' + uri.pathname);
+          response.writeHead(200, {
+            'Content-length': file.length
+          });
+          stream = file.stream(true);
+          stream.on('data', function(data){
+            response.write(data);
+          });
+          stream.on('end', function(){
+            winston.info('(' + ruid + ') File downloaded');
+            response.end();
+          });
         });
-        stream.on('end', function(){
-          console.log('Ready.');
-          response.end();
-        });
-      });
-    // Save file
-    }else if(request.method == "POST"){
+      };
+    }else if(request.method == "PUT"){
+      winston.info('(' + ruid + ') Uploading file ' + uri.pathname);
       var gs = new mongo.GridStore(db, path, "w");
       var pool = [];
       var done = false;
@@ -58,7 +85,6 @@ var start = function(db, Config){
         done = true;
       });
       gs.open(function(error, file){
-        console.log('Uploading file: ' + error);
         for(x in pool){
           if(pool[x]){
             file.write(pool[x], function() {});
@@ -67,17 +93,16 @@ var start = function(db, Config){
         pool = [];
         if(done){
           file.close(function(){
-            console.log('Ready.');
+            winston.info('(' + ruid + ') File uploaded');
             response.end('OK');
           });
         }else{
           request.on('data', function(data){
-            console.log('Writing data.' + data);
             file.write(data, function(){});
           });
           request.on('end', function(){
             file.close(function(){
-              console.log('Ready.');
+              winston.info('(' + ruid + ') File uploaded');
               response.end('OK');
             });
           });
@@ -85,28 +110,36 @@ var start = function(db, Config){
       });
     // Delete file
     }else if(request.method == "DELETE"){
-      console.log('Deleting file.');
+      winston.info('(' + ruid + ') Deleting file ' + uri.pathname);
       mongo.GridStore.unlink(db, path, function(){
-        console.log('Ready.');
+        winston.info('(' + ruid + ') File deleted');
         response.end('OK');
       });
     }else{
+      winston.info('(' + ruid + ') Invalid request.');
       response.end("Unhandled!");
     }
   });
 
+  winston.info('Starting HTTP server on port ' + Config.port);
   app.listen(Config.port, function() {
-    console.log("Listening on port " + Config.port);
+    winston.info('Listening on port ' + Config.port);
   });
 
 };
 
-fs.readFile('storage.json', function(err, data) {
+var config_file = "/etc/wildcloud/storage.json";
+if(!Path.existsSync(config_file)){
+  config_file = "storage.json";
+};
 
+winston.info("Loading configuration file " + config_file);
+
+fs.readFile(config_file, function(err, data) {
   Config = JSON.parse(data);
-
+  winston.info("Connecting to database");
   mongo.connect(Config.mongodb.url, function(error, db){
+    winston.info('Database connected');
     start(db, Config);
   });
-
 });
