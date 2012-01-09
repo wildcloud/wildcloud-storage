c16c5ce8ceb59897eb37f57702b7e1e722d5f907
Marek Jelen <marek@jelen.biz>
2012-01-09 20:49:33 +0100

    Apache 2 licence & small cleanup

--- fs.js
+++ fs.js
@@ -1,102 +1,119 @@
+/*
+ Copyright 2011 Marek Jelen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 var FS = require('fs');
+
 var Path = require('path');
 
-Driver = function() {
-  var self = this;
+Driver = function () {
+    var self = this;
 
-  // Setup
-  self.setup = function(callback){
-    callback();
-  };
-
-  // Handles file listing
-  self.handleListFiles = function(response, ruid, appid){
-    self.log.debug('(' + ruid + ') Listing files for ' + appid);
-    FS.readdir('data/' + appid, function(error, files){
-      if(error){
-        response.writeHead(404, {} );
-        response.end();
-        return;
-      };
-      for(file in files){
-        response.write(files[file] + "\n");
-      };
-      response.end();
-    });
-  };
-
-  // Handles file downloads
-  self.handleDownload = function(response, ruid, appid, path, request){
-    self.log.debug('(' + ruid + ') Downloading file ' + path + ' for ' + appid);
-    var filepath = self.configuration.fs.dir + '/' + appid + '/' + path;
-    FS.stat(filepath, function(error, stat){
-      if(error){
-        response.writeHead(404, {} );
-        response.end();
-        return;
-      };
-      stream = FS.createReadStream(filepath);
-      response.writeHead(200, {
-        'Content-length': stat.size
-      });
-      stream.pipe(response);
-      response.on('close', function(){
-        self.log.debug('(' + ruid + ') File downloaded');
-      });
-    });
-  };
-
-
-  // Handles file uploads
-  self.handleUpload = function(response, ruid, appid, path, request){
-    self.log.debug('(' + ruid + ') Uploading file ' + path + ' for ' + appid);
-    var dir = self.configuration.fs.dir + '/' + appid;
-    var filepath =  dir + '/' + path;
-    var buffer = [];
-    var onData = function(chunk){
-      buffer.push(chunk);
+    // Setup
+    self.setup = function (callback) {
+        callback();
     };
-    request.addListener('data', onData);
-    request.pause();
-    FS.mkdir(dir, function(error){
-      if(error){
-        response.writeHead(500, {});
-        response.end();
-      };
-      stream = FS.createWriteStream(filepath);
-      request.removeListener('data', onData);
-      for(chunk in buffer){
-        stream.write(buffer[chunk]);
-      };
-      request.resume();
-      request.pipe(stream, { end: false });
-      stream.on('error', function(error){
-        response.end(error.toString());
-      });
-      request.on('end', function(){
-        stream.destroySoon();
-        self.log.debug('(' + ruid + ') File uploaded');
-        response.end('OK');
-      });
-    });
-  };
 
-  // Handles file deletions
-  self.handleDelete = function(response, ruid, appid, path){
-    self.log.debug('(' + ruid + ') Deleting file ' + path + ' for ' + appid);
-    var filepath = self.configuration.fs.dir + '/' + appid + '/' + path;
-    FS.stat(filepath, function(error, stat){
-      if(error){
-        response.writeHead(404, {} );
-        response.end();
-        return;
-      };
-      FS.unlink(filepath, function(){
-        self.log.debug('(' + ruid + ') File deleted');
-        response.end('OK');
-      });
-    });
-  };
+    // Handles file listing
+    self.handleListFiles = function (response, ruid, appid) {
+        self.log.debug('(' + ruid + ') Listing files for ' + appid);
+        FS.readdir('data/' + appid, function (error, files) {
+            if (error) {
+                response.writeHead(404, {});
+                response.end();
+                return;
+            }
+            for (file in files) {
+                response.write(files[file] + "\n");
+            }
+            response.end();
+        });
+    };
+
+    // Handles file downloads
+    self.handleDownload = function (response, ruid, appid, path, request) {
+        self.log.debug('(' + ruid + ') Downloading file ' + path + ' for ' + appid);
+        var filepath = self.configuration.fs.dir + '/' + appid + '/' + path;
+        FS.stat(filepath, function (error, stat) {
+            if (error) {
+                response.writeHead(404, {});
+                response.end();
+                return;
+            }
+            stream = FS.createReadStream(filepath);
+            response.writeHead(200, {
+                'Content-length':stat.size
+            });
+            stream.pipe(response);
+            response.on('close', function () {
+                self.log.debug('(' + ruid + ') File downloaded');
+            });
+        });
+    };
+
+
+    // Handles file uploads
+    self.handleUpload = function (response, ruid, appid, path, request) {
+        self.log.debug('(' + ruid + ') Uploading file ' + path + ' for ' + appid);
+        var dir = self.configuration.fs.dir + '/' + appid;
+        var filepath = dir + '/' + path;
+        var buffer = [];
+        var onData = function (chunk) {
+            buffer.push(chunk);
+        };
+        request.addListener('data', onData);
+        request.pause();
+        FS.mkdir(dir, function (error) {
+            if (error) {
+                response.writeHead(500, {});
+                response.end();
+            }
+            stream = FS.createWriteStream(filepath);
+            request.removeListener('data', onData);
+            for (chunk in buffer) {
+                stream.write(buffer[chunk]);
+            }
+            request.resume();
+            request.pipe(stream, { end:false });
+            stream.on('error', function (error) {
+                response.end(error.toString());
+            });
+            request.on('end', function () {
+                stream.destroySoon();
+                self.log.debug('(' + ruid + ') File uploaded');
+                response.end('OK');
+            });
+        });
+    };
+
+    // Handles file deletions
+    self.handleDelete = function (response, ruid, appid, path) {
+        self.log.debug('(' + ruid + ') Deleting file ' + path + ' for ' + appid);
+        var filepath = self.configuration.fs.dir + '/' + appid + '/' + path;
+        FS.stat(filepath, function (error, stat) {
+            if (error) {
+                response.writeHead(404, {});
+                response.end();
+                return;
+            }
+            FS.unlink(filepath, function () {
+                self.log.debug('(' + ruid + ') File deleted');
+                response.end('OK');
+            });
+        });
+    };
 
 };
 
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-// Copyright (C) 2011 Marek Jelen
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ Copyright 2011 Marek Jelen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 var Http = require('http');
 var Url = require('url');
@@ -19,115 +20,115 @@ var FS = require('fs');
 var Path = require('path');
 var winston = require('winston');
 
-Storage = function() {
-  // Make the scope accessible
-  self = this;
+Storage = function () {
+    // Make the scope accessible
+    self = this;
 
-  // Some basics
-  this.config_file = '/etc/wildcloud/storage.json';
-  this.log = winston;
-  this.ruid = 0;
+    // Some basics
+    this.config_file = '/etc/wildcloud/storage.json';
+    this.log = winston;
+    this.ruid = 0;
 
-  // Starts the server
-  this.start = function(){
-    self.log.cli();
-    self.log.info('Bootstraping');
-    self.loadConfiguration();
-    self.loadDriver();
-  };
-
-  // Loads configuration from file
-  this.loadConfiguration = function(){
-    if(!Path.existsSync(self.config_file)){
-      self.config_file = "./storage.json";
+    // Starts the server
+    this.start = function () {
+        self.log.cli();
+        self.log.info('Bootstraping');
+        self.loadConfiguration();
+        self.loadDriver();
     };
-    self.log.info("Loading configuration file " + self.config_file);
-    self.configuration = JSON.parse(FS.readFileSync(self.config_file));
-  };
 
-  // Load driver for data storage
-  this.loadDriver = function(){
-    self.log.info("Loading driver '" + self.configuration.driver + "'");
-    var driverClass = require('./' + self.configuration.driver + '.js').driver;
-    var driver = new driverClass();
-    // Pass environment to driver
-    driver.configuration = self.configuration;
-    driver.log = self.log;
-    // Mount handlers
-    self.handleListFiles = driver.handleListFiles;
-    self.handleDownload = driver.handleDownload;
-    self.handleUpload = driver.handleUpload;
-    self.handleDelete = driver.handleDelete;
-    // Setup driver
-    driver.setup(this.startHttpServer);
-  };
-
-  // Handles fatal errors
-  this.handleError = function(message){
-    self.log.error(message);
-    process.exit(1);
-  };
-
-
-  // Start HTTP server
-  this.startHttpServer = function(){
-    self.log.info('Starting HTTP server on port ' + self.configuration.port);
-    self.application().listen(self.configuration.port, function() {
-      winston.info('Listening on port ' + self.configuration.port);
-    });
-  };
-
-  // Bootstrap HTTP application
-  this.application = function(){
-    return Http.createServer(this.handleHttpRequest);
-  };
-
-  // Handles incomming HTTP requests
-  this.handleHttpRequest = function(request, response){
-    // Identify request
-    var ruid = self.getRuid(request);
-    winston.debug('(' + ruid + ') New connection');
-    // Parse URI
-    var uri = Url.parse(request.url, true);
-    // ID of application
-    var appid = request.headers['x-appid'];
-    winston.debug('(' + ruid + ') Requests Appid = ' + appid);
-    // Path to work with
-    var path = uri.pathname.substring(1);
-    if(request.method == "GET"){
-      // GET requests
-      if(uri.pathname == "/_list_files"){
-        // Listing of files
-        self.handleListFiles(response, ruid, appid);
-      }else{
-        // File downloads
-        self.handleDownload(response, ruid, appid, path, request);
-      };
-      return;
+    // Loads configuration from file
+    this.loadConfiguration = function () {
+        if (!Path.existsSync(self.config_file)) {
+            self.config_file = "./storage.json";
+        }
+        self.log.info("Loading configuration file " + self.config_file);
+        self.configuration = JSON.parse(FS.readFileSync(self.config_file));
     };
-    if(request.method == "PUT"){
-      // File uploads
-      self.handleUpload(response, ruid, appid, path, request);
-      return;
-    };
-    if(request.method == "DELETE"){
-      // File deletions
-      self.handleDelete(response, ruid, appid, path);
-      return;
-    };
-    // Unhandled requests
-    winston.info('(' + ruid + ') Invalid request.');
-    response.end("Unhandled!");
-  };
 
-  // Generated unique ID per request
-  this.getRuid = function(request){
-    self.ruid++;
-    if(self.ruid == 5000) self.ruid = 1;
-    return self.ruid;
-  };
+    // Load driver for data storage
+    this.loadDriver = function () {
+        self.log.info("Loading driver '" + self.configuration.driver + "'");
+        var driverClass = require('./' + self.configuration.driver + '.js').driver;
+        var driver = new driverClass();
+        // Pass environment to driver
+        driver.configuration = self.configuration;
+        driver.log = self.log;
+        // Mount handlers
+        self.handleListFiles = driver.handleListFiles;
+        self.handleDownload = driver.handleDownload;
+        self.handleUpload = driver.handleUpload;
+        self.handleDelete = driver.handleDelete;
+        // Setup driver
+        driver.setup(this.startHttpServer);
+    };
 
-  return this;
+    // Handles fatal errors
+    this.handleError = function (message) {
+        self.log.error(message);
+        process.exit(1);
+    };
+
+
+    // Start HTTP server
+    this.startHttpServer = function () {
+        self.log.info('Starting HTTP server on port ' + self.configuration.port);
+        self.application().listen(self.configuration.port, function () {
+            winston.info('Listening on port ' + self.configuration.port);
+        });
+    };
+
+    // Bootstrap HTTP application
+    this.application = function () {
+        return Http.createServer(this.handleHttpRequest);
+    };
+
+    // Handles incomming HTTP requests
+    this.handleHttpRequest = function (request, response) {
+        // Identify request
+        var ruid = self.getRuid(request);
+        winston.debug('(' + ruid + ') New connection');
+        // Parse URI
+        var uri = Url.parse(request.url, true);
+        // ID of application
+        var appid = request.headers['x-appid'];
+        winston.debug('(' + ruid + ') Requests Appid = ' + appid);
+        // Path to work with
+        var path = uri.pathname.substring(1);
+        if (request.method == "GET") {
+            // GET requests
+            if (uri.pathname == "/_list_files") {
+                // Listing of files
+                self.handleListFiles(response, ruid, appid);
+            } else {
+                // File downloads
+                self.handleDownload(response, ruid, appid, path, request);
+            }
+            return;
+        }
+        if (request.method == "PUT") {
+            // File uploads
+            self.handleUpload(response, ruid, appid, path, request);
+            return;
+        }
+        if (request.method == "DELETE") {
+            // File deletions
+            self.handleDelete(response, ruid, appid, path);
+            return;
+        }
+        // Unhandled requests
+        winston.info('(' + ruid + ') Invalid request.');
+        response.end("Unhandled!");
+    };
+
+    // Generated unique ID per request
+    this.getRuid = function (request) {
+        self.ruid++;
+        if (self.ruid == 5000) self.ruid = 1;
+        return self.ruid;
+    };
+
+    return this;
 };
 
 storage = new Storage();
